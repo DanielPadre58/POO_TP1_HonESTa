cf622507b126abc7a0c40911ed02787dbd91de3e
AndreSantos4 <149972193+AndreSantos4@users.noreply.github.com>
2025-03-28 22:04:35 +0000

    Update Cupao.java

--- HonESTa/src/cliente/Cupao.java
+++ HonESTa/src/cliente/Cupao.java
@@ -1,6 +1,20 @@
 package cliente;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import comercio.ProdutoInfo;
+
+
+/*Cupao{
+	final String id;
+	final byte desconto;
+	final String descricaoProdutoValidos;
+	LocalDate validade
+	ArrayList<Produto>
+}
+ */
 
 /**
  * Classe que representa um cupão emitido pela cadeia de lojas HonESta. Este
@@ -8,26 +22,98 @@ import java.time.LocalDate;
  * direito a um desconto (em cartão) na compra dos produtos que abrange.
  */
 public class Cupao {
-    /**
-     * Indica se o cupão está válido no dia de hoje, isto é, se o dia de hoje está
-     * dentro do prazo de utilização.
-     * 
-     * @return true se está dentro do prazo de utilização
-     */
-    public boolean estaValido() {
-        // TODO implementar este método
-        return false;
+    private final String id;
+    private final String descricao;
+    private final byte desconto;
+    private final LocalDate validade;
+    private List <ProdutoInfo> produtos;
+
+    public Cupao(String id, String descricao, byte desconto, LocalDate validade,
+        List<ProdutoInfo> produtos) {
+
+        verificarId(id);
+        this.id = id;
+        verificarDescricao(descricao);
+        this.descricao = descricao;
+        verificarCupao(desconto);
+        this.desconto = desconto;
+        verificarValidade(validade);
+        this.validade = validade;
+        verificarProdutos(produtos);
+        this.produtos = produtos;
     }
 
-    /**
-     * Indica se o cupão está válido num dado dia, isto é, se esse dia está dentro
-     * do prazo de utilização.
-     * 
-     * @param data dia em que se pretende verificar se o cupão está válido
-     * @return true se a data está dentro do prazo de utilização
-     */
+    // Verificações
+
+    public void verificarId(String id) {
+        if (id == null || id.isEmpty() || id.isBlank()) {
+            throw new IllegalArgumentException("O id não pode ser vazio ou nulo!");
+        }
+    }
+
+    public void verificarDescricao(String descricao) {
+        if (descricao == null || descricao.isEmpty() || descricao.isBlank()) {
+            throw new IllegalArgumentException("A descrição não pode ser vazia ou nula!");
+        }
+    }
+
+    public void verificarCupao(byte desconto) {
+        if (desconto <= 0 || desconto >= 100) {
+            throw new IllegalArgumentException("O desconto não pode ser negativo ou igual a 0 ou maior ou igual que 100!");
+        }
+    }
+
+    public void verificarValidade(LocalDate validade) {
+        if (validade == null || validade.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("A validade não pode ser nula ou anterior a de hoje!");
+        }
+    }
+
+    public void verificarProdutos(List<ProdutoInfo> produtos) {
+        if (produtos == null || produtos.isEmpty()) {
+            throw new IllegalArgumentException("A lista de produtos não pode ser vazia ou nula!");
+        }
+    }
+
+    // Getters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public byte getDesconto() {
+        return desconto;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public List<ProdutoInfo> getProdutos() {
+        return produtos;
+    }
+
+    // Setters
+
+    public void setProdutos(ArrayList<ProdutoInfo> produtos) {
+        this.produtos = produtos;
+    }
+
+    public boolean estaValido() {
+        if (validade.isBefore(LocalDate.now())) {
+            return false;
+        }
+        return true;
+    }
+
     public boolean estaValido(LocalDate data) {
-        // TODO implementar este método
+        if (data.isBefore(validade) || data.equals(validade)) {
+            return true;
+        }
         return false;
     }
 
