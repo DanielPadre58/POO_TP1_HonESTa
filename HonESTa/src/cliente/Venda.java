196d375a886b04cc800b8b18a50d082ea353fe09
DanielPadre58 <2kupwhite@gmail.com>
2025-03-28 20:05:16 +0000

    O codigo original sem alterações

---
+++ HonESTa/src/cliente/Venda.java
@@ -0,0 +1,8 @@
+package cliente;
+
+/**
+ * Classe responsável por armazenar os produtos vendidos numa venda. Tem a data
+ * em que foi realizada, os produtos e os cupões aplicados.
+ */
+public class Venda {
+}
